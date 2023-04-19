dae754f5c5321137d9315c6521606b459e431453
jfiliplic <97060179+jfiliplic@users.noreply.github.com>
2023-04-19 11:53:02 +0200

    navigation js

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -2,12 +2,12 @@ const navigationToggle = document.querySelector(".menu-btn");
 const navigation = document.querySelector("nav");
 const welcome = document.querySelector(".welcome");
 
-navigationToggle.addEventListener("click", (_) => {
+navigationToggle.addEventListener("click", ()  => {
   document.body.classList.toggle("nav-is-open");
   welcome.style.transition = "transform ease-out 250ms";
 });
 
-navigation.addEventListener("click", (_) => {
+navigation.addEventListener("click", () => {
   document.body.classList.remove("nav-is-open");
   welcome.style.transition = "0ms";
 });
