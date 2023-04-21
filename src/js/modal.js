531bfd6ab5c676b4bc3fb63cb4169e7ce0385b6c
jfiliplic <97060179+jfiliplic@users.noreply.github.com>
2023-04-21 11:18:57 +0200

    js for project modal

---
+++ src/js/modal.js
@@ -0,0 +1,40 @@
+const portfolioContainer = document.querySelector(".portfolio-items");
+
+portfolioContainer.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const modalLinkClick = event.target.closest(".portfolio-link");
+
+  if (!modalLinkClick) {
+    return;
+  } else {
+    const modal = modalLinkClick.parentNode.nextElementSibling;
+    const closeButton = modal.querySelector(".modal-close");
+
+    let modalOpen = () => {
+      modal.classList.add("is-open");
+      modal.style.animation = "modalFadeIn 1s forwards";
+      document.body.style.overflowY = "hidden";
+    };
+
+    modalOpen();
+
+    let modalClose = () => {
+      modal.classList.remove("is-open");
+      modal.removeEventListener("animationend", modalClose);
+      document.body.style.overflowY = "scroll";
+    };
+
+    closeButton.addEventListener("click", () => {
+      modal.style.animation = "modalFadeOut 1s forwards";
+      modal.addEventListener("animationend", modalClose);
+    });
+
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "Escape") {
+        modal.style.animation = "modalFadeOut 1s forwards";
+        modal.addEventListener("animationend", modalClose);
+      }
+    });
+  }
+});
