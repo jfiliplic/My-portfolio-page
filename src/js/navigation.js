24e2f36203de64a34be0aea4ede771346b21525a
mitjapotocin <mitja.potocin@gmail.com>
2023-04-22 08:25:56 +0200

    Simple nav toggle

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -2,54 +2,23 @@ const navigationToggle = document.querySelector(".menu-btn");
 const navigation = document.querySelector("nav");
 const welcome = document.querySelector(".welcome");
 
-navigationToggle.addEventListener("click", () => {
+navigationToggle.addEventListener("click", (e) => {
+  e.stopPropagation();
   document.body.classList.toggle("nav-is-open");
-  welcome.classList.toggle("welcome-to-right");
-  welcome.style.transition = "transform ease-out 250ms";
-  if (welcome.classList.contains("welcome-to-right")) {
-    const welcomeRight = document.querySelector(".welcome-to-right");
-    setTimeout(
-      () =>
-        welcomeRight.addEventListener(
-          "click",
-          () => {
-            document.body.classList.remove("nav-is-open");
-            welcome.classList.remove("welcome-to-right");
-            welcome.style.transition = "transform ease-out 250ms";
-          },
-          { once: true }
-        ),
-      500
-    );
+});
+
+welcome.addEventListener("click", () => {
+  if (document.body.classList.contains("nav-is-open")) {
+    document.body.classList.remove("nav-is-open");
   }
 });
 
-// const welcomeRight = document.querySelector(".welcome-to-right");
-// if (welcomeRight) {
-//   setTimeout(
-//     () =>
-//       welcomeRight.addEventListener(
-//         "click",
-//         () => {
-//           document.body.classList.remove("nav-is-open");
-//           welcome.classList.remove("welcome-to-right");
-//           welcome.style.transition = "transform ease-out 250ms";
-//         },
-//         { once: true }
-//       ),
-//     1000
-//   );
-// }
-
-navigation.addEventListener("click", () => {
+navigation.addEventListener("click", (e) => {
   document.body.classList.remove("nav-is-open");
-  welcome.classList.remove("welcome-to-right");
-  welcome.style.transition = "0ms";
 });
 
 document.addEventListener("keydown", (event) => {
   if (event.key === "Escape") {
     document.body.classList.remove("nav-is-open");
-    welcome.classList.remove("welcome-to-right");
   }
 });
